9f5058a561b8c3b191e24e9d65235f4e3b7d07c2
FantasticFreddy <FantasticFreddy@users.noreply.github.com>
2016-08-14 11:18:25 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## results in an object, makeCacheMatrix, that contains four functions: set(), get(), 
+## setmatrix(), and getmatrix(). It also includes the two data objects, x and m.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <-function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <-function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        list (set = set, get = get,
+              setmatrix = setmatrix,
+              getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## Computes the matrix inversion  using solve.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix()
+        if (!is.null(m)) {
+                message("cached data")
+                return(m)
+        }
+        matrix <- x$get()
+        m <- solve(matrix, ...)
+        x$setmatrix(m)
+        m
 }
